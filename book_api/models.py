35632e523debcfe4c7ddac4ed90982dcf75d510c
eleneaa <lenatisenko4@gmail.com>
2024-04-12 23:15:25 +0500

    first

---
+++ book_api/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+
+
+class Book(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    city = models.CharField(max_length=15)
+    number = models.CharField(max_length=25)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user
